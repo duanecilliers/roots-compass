0e8256e1d91c60f4c63ce14964a3fc7bcfb6e1f4
Duane Cilliers <duanecilliers@gmail.com>
2014-02-20 16:00:08 +0200

    strip roots-sass of everying uncessary and start with new Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,122 +1,102 @@
-'use strict';
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+	'use strict';
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'assets/js/*.js',
-        '!assets/js/scripts.min.js'
-      ]
-    },
-    sass: {
-      dist: {
-        options: {
-          style: 'compressed',
-					compass: true,
-					// Source maps are available, but require Sass 3.3.0 to be installed
-					// https://github.com/gruntjs/grunt-contrib-sass#sourcemap
-					sourcemap: false
-        },
-        files: {
-          'assets/css/main.min.css': [
-            'assets/sass/app.scss'
-          ]
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'assets/js/scripts.min.js': [
-            'assets/js/plugins/bootstrap/transition.js',
-            'assets/js/plugins/bootstrap/alert.js',
-            'assets/js/plugins/bootstrap/button.js',
-            'assets/js/plugins/bootstrap/carousel.js',
-            'assets/js/plugins/bootstrap/collapse.js',
-            'assets/js/plugins/bootstrap/dropdown.js',
-            'assets/js/plugins/bootstrap/modal.js',
-            'assets/js/plugins/bootstrap/tooltip.js',
-            'assets/js/plugins/bootstrap/popover.js',
-            'assets/js/plugins/bootstrap/scrollspy.js',
-            'assets/js/plugins/bootstrap/tab.js',
-            'assets/js/plugins/bootstrap/affix.js',
-            'assets/js/plugins/*.js',
-            'assets/js/_*.js'
-          ]
-        },
-        options: {
-          // JS source map: to enable, uncomment the lines below and update sourceMappingURL based on your install
-          // sourceMap: 'assets/js/scripts.min.js.map',
-          // sourceMappingURL: '/app/themes/roots/assets/js/scripts.min.js.map'
-        }
-      }
-    },
-    version: {
-      options: {
-        file: 'lib/scripts.php',
-        css: 'assets/css/main.min.css',
-        cssHandle: 'roots_main',
-        js: 'assets/js/scripts.min.js',
-        jsHandle: 'roots_scripts'
-      }
-    },
-    watch: {
-      sass: {
-        files: [
-          'assets/sass/*.scss',
-          'assets/sass/bootstrap/*.scss'
-        ],
-        tasks: ['sass', 'version']
-      },
-      js: {
-        files: [
-          '<%= jshint.all %>'
-        ],
-        tasks: ['jshint', 'uglify', 'version']
-      },
-      livereload: {
-        // Browser live reloading
-        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
-        options: {
-          livereload: false
-        },
-        files: [
-          'assets/css/main.min.css',
-          'assets/js/scripts.min.js',
-          'templates/*.php',
-          '*.php'
-        ]
-      }
-    },
-    clean: {
-      dist: [
-        'assets/css/main.min.css',
-        'assets/js/scripts.min.js'
-      ]
-    }
-  });
+	// Load all Grunt tasks using matchdep
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-wp-version');
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			options: {
+				livereload: true
+			},
+			compass: {
+				files: ['assets/sass/*.{scss,sass}'],
+				tasks: [] // @TODO
+			},
+			scripts: {
+				files: ['assets/js/*.js', '!assets/js/*.min.js'],
+				tasks: ['jshint', 'requirejs'],
+				options: {
+					debounceDelay: 500
+				}
+			},
+			markup: {
+				files: ['*.php']
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc',
+				ignores: ['assets/js/*.min.js']
+			},
+			files: ['assets/js/*.js', 'assets/js/test/**/*.js'],
+			grunt: {
+				options: {
+					jshintrc: '.gruntjshintrc'
+				},
+				files: {
+					src: ['Gruntfile.js']
+				}
+			}
+		},
+		modernizr: {
+			dists: {
+				devFile: "bower_components/modernizr/modernizr.js",
+				outputFile: "assets/js/_modernizr.js",
+				// Based on default settings on http://modernizr.com/download/
+				extra: {
+					"shiv": true,
+					"printshiv": false,
+					"load": true,
+					"mq": false,
+					"cssclasses": true
+				},
+				// Based on default settings on http://modernizr.com/download/
+				extensibility: {
+					"addtest": false,
+					"prefixed": false,
+					"teststyles": false,
+					"testprops": false,
+					"testallprops": false,
+					"hasevents": false,
+					"prefixes": false,
+					"domprefixes": false
+				},
+				files: {
+					src: ['assets/js/*.js', 'assets/sass/*.{scss,sass}']
+				}
+			}
+		},
+		requirejs: {
+			compile: {
+				options: {
+					baseUrl: './',
+					mainConfigFile: 'assets/js/_config.js',
+					name: 'bower_components/almond/almond.js',
+					out: 'assets/js/scripts.min.js',
+					done: function (done, output) {
+						var duplicates = require('rjs-build-analysis').duplicates(output);
 
-  // Register tasks
-  grunt.registerTask('default', [
-    'clean',
-    'sass',
-    'uglify',
-    'version'
-  ]);
-  grunt.registerTask('dev', [
-    'watch'
-  ]);
+						if (duplicates.length > 0) {
+							grunt.log.subhead('Duplicates found in require build');
+							grunt.log.warn(duplicates);
+							done(new Error('r.js built duplicate modules, please check the excludes option.'));
+						}
 
+						done();
+					}
+				}
+			},
+			optimize: 'uglify',
+			uglify: {
+				toplevel: true,
+				ascii_only: true,
+				beautify: true,
+				max_line_length: 1000,
+				no_mange: true
+			}
+		}
+	});
 };
--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-# [Roots Theme](http://roots.io/)
+# Roots Compass Theme
 
 [![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
 
-Roots is a WordPress starter theme based on [HTML5 Boilerplate](http://html5boilerplate.com/) & [Bootstrap](http://getbootstrap.com/) that will help you make better themes.
+Roots Compass is a fork of [Roots](https://github.com/roots/roots) a WordPress starter theme based on [HTML5 Boilerplate](http://html5boilerplate.com/) & [Bootstrap](http://getbootstrap.com/) that will help you make better themes.
 
-* Source: [https://github.com/roots/roots](https://github.com/roots/roots)
-* Home Page: [http://roots.io/](http://roots.io/)
-* Twitter: [@retlehs](https://twitter.com/retlehs)
-* Newsletter: [Subscribe](http://roots.io/subscribe/)
-* Forum: [http://discourse.roots.io/](http://discourse.roots.io/)
+* Original: [https://github.com/roots/roots](https://github.com/roots/roots)
+* Source: [https://github.com/duanecilliers/roots-compass](https://github.com/duanecilliers/roots-compass)
+* Twitter: [@retlehs](https://twitter.com/duanecilliers)
 
 ## Installation
 
-Clone the git repo - `git clone git://github.com/roots/roots.git` - or [download it](https://github.com/roots/roots/zipball/master) and then rename the directory to the name of your theme or website. [Install Grunt](http://gruntjs.com/getting-started), and then install the dependencies for Roots contained in `package.json` by running the following from the Roots theme directory:
+Clone the git repo - `git clone git://github.com/roots/roots.git` - or [download it](https://github.com/duanecilliers/roots-compass/zipball/master) and then rename the directory to the name of your theme or website. [Install Grunt](http://gruntjs.com/getting-started), and then install the dependencies contained in `package.json` by running the following from the theme directory:
 
 ```
 npm install
@@ -36,34 +34,32 @@ Edit `lib/init.php` to setup custom navigation menus and post thumbnail sizes.
 
 * [Roots 101](http://roots.io/roots-101/) — A guide to installing Roots, the files and theme organization
 * [Theme Wrapper](http://roots.io/an-introduction-to-the-roots-theme-wrapper/) — Learn all about the theme wrapper
-* [Build Script](http://roots.io/using-grunt-for-wordpress-theme-development/) — A look into the Roots build script powered by Grunt
 * [Roots Sidebar](http://roots.io/the-roots-sidebar/) — Understand how to display or hide the sidebar in Roots
 
 ## Features
 
-* Organized file and template structure
-* HTML5 Boilerplate's markup along with ARIA roles and microformat
-* Bootstrap
-* [Grunt build script](http://roots.io/using-grunt-for-wordpress-theme-development/)
-* [Theme activation](http://roots.io/roots-101/#theme-activation)
-* [Theme wrapper](http://roots.io/an-introduction-to-the-roots-theme-wrapper/)
-* Root relative URLs
-* Cleaner HTML output of navigation menus
-* Cleaner output of `wp_head` and enqueued scripts/styles
-* Nice search (`/search/query/`)
-* Image captions use `<figure>` and `<figcaption>`
-* Example vCard widget
-* Posts use the [hNews](http://microformats.org/wiki/hnews) microformat
-* [Multilingual ready](http://roots.io/wpml/) (Brazilian Portuguese, Bulgarian, Catalan, Danish, Dutch, English, Finnish, French, German, Hungarian, Indonesian, Italian, Korean, Macedonian, Norwegian, Polish, Russian, Simplified Chinese, Spanish, Swedish, Traditional Chinese, Turkish, Vietnamese, Serbian)
+[All Roots features](https://github.com/roots/roots#features) plus some extra stuff:
+
+* Package management with Bower
+* Automatically compile Sass
+* LiveReload the browser (with extension)
+* AMD file and module loader with Require.js (not recommended for publicly released themes due to the way WordPress enqueues scripts and styles)
+* Automated custom lean Modernizr builds
+* Automatically autoprefix CSS. Uses the Can I Use database
+* Automatically lint, concatenate and minify your JavaScript (with source maps)
+* Automatically versions your styles and scripts with an md5 hash (rev the filename, [some proxies will never cache an asset if it has a querystring](http://www.stevesouders.com/blog/2008/08/23/revving-filenames-dont-use-querystring/.)
+* Awesome Image Optimization (via OptiPNG, pngquant, jpegtran and gifsicle)
 
 ## Contributing
+
+Please note that this is a fork of [Roots](https://github.com/roots/roots). I am happy to help where I can, specifically where this theme differs from Roots.
 
 Everyone is welcome to help [contribute](CONTRIBUTING.md) and improve this project. There are several ways you can contribute:
 
 * Reporting issues (please read [issue guidelines](https://github.com/necolas/issue-guidelines))
 * Suggesting new features
 * Writing or refactoring code
-* Fixing [issues](https://github.com/roots/roots/issues)
+* Fixing [issues](https://github.com/duanecilliers/roots-compass/issues)
 * Replying to questions on the [forum](http://discourse.roots.io/)
 
 ## Support
